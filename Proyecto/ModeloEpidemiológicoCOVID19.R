0ffc551db2f286cabceeea8a446a9ff12bb8457b
Monica Alvarez <60207713+monicaleja99@users.noreply.github.com>
2020-05-23 14:33:31 -0500

    Modelo SIR completo

--- Proyecto/ModeloEpidemiológicoCOVID19.R
+++ Proyecto/ModeloEpidemiológicoCOVID19.R
@@ -13,13 +13,13 @@ ruta = file.choose()
 database = read.csv(ruta)
 dias=nrow(database)-1
 
-# Define UI for application that draws a histogram
+#Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
+    #Application title
     titlePanel("Modelo SI"),
 
-    # Sidebar with a slider input for number of bins 
+    #Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             sliderInput("poblacion",
@@ -54,7 +54,7 @@ ui <- fluidPage(
                         value = dias),
         ),
         
-        # Show a plot of the generated distribution
+        #Show a plot of the generated distribution
         mainPanel(
             actionButton("botonCalcularSI", "Calcular SI"),
             actionButton("botonCalcularSIR", "Calcular SIR"),
@@ -66,7 +66,7 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+#Define server logic required to draw a histogram
 server <- function(input, output, session) {
     
     
@@ -85,106 +85,86 @@ server <- function(input, output, session) {
     CalcularEDOSI <- function(){
         output$distPlot <- renderPlot({
             tf = input$tiempodias
-            #ESTADO INICIAL, mover deslizadores solo cambia estos valores, lo iniciales. 
-            init <- c(S = input$poblacion , # Susceptibles
-                      I = input$infectados)  # Infectados
+            
+            init <- c(S = input$poblacion , #Susceptibles
+                      I = input$infectados)  #Infectados
 
-            param<-c(beta = input$transmision) #Hay que cambiar esto para que se ingrese con valores de los deslizadores. TOCA INVESTIGAR QUE VALORES SON MASO REALES. 
-            #crear la funcion con las ODE.TOCA MIRAR SI ESTAN BIEN, Ellos interpretan gamma como probabilidad de infeccion y Wikipedia lo define como tiempo promedio de infeccion.
-            si <- function(times, init, param) #Por eso toca mirar bien como se estan construyendo esas ED
+            param<-c(beta = input$transmision) 
+            si <- function(times, init, param)
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- - beta*S*I/(S+I)
                          dI <- beta*S*I/(S+I)
-                         #resultados de las tasas de cambio    
                          return(list(c(dS, dI)))
                      })
             }
             
-            #intervalo de tiempo y resolucion
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
-            simulacionM1SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "rk4"))      #Aqui se solucionan, mejor DEJAR ESTAS TRES LINEAS QUIETAS
-            #Esta orden nos permite hacer referencia de manera directa a #las columnas de los resultados obtenidos
+            simulacionM1SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "rk4"))
             attach(simulacionM1SI.si)
 
-            #Representamos graficamente los resultados obtenidos CON ESTO SI PODEMOS JUGAR UN POCO MAS, HAY QUE CAMBIAR LOS TITULOS Y ESAS COSAS. 
+            #Gráficas
             plot(times, S, type="l", col="blue", ylim=c(0,sum(init)), xlab="Tiempo (en días)", ylab="Numero de host",main = "Metodo 1: Runge Kutta 4")
             lines(times, I, type="l", col="red")
-            #title("Modelo SI")
             legend(x = "topright", legend=c("Susceptibles", "Infectados"), col=c("blue", "red"), lty=rep(1, 2)) 
         })
         
         output$distPlot2 <- renderPlot({
             tf = input$tiempodias
             
-            init <- c(S = input$poblacion , # Suceptibles 
-                      I = input$infectados)  # Infectados
+            init <- c(S = input$poblacion , #Suceptibles 
+                      I = input$infectados)  #Infectados
 
             param<-c(beta = input$transmision)
-            #crear la funcion con las ODE
+            
             si <- function(times, init, param) 
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- -beta*S*I/(S+I)
                          dI <- beta*S*I/(S+I)
-                         #resultados de las tasas de cambio    
+                            
                          return(list(c(dS, dI)))
                      })
             }
             
-            #intervalo de tiempo y resolucion
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM2SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "euler"))
-            #Esta orden nos permite hacer referencia de manera directa a #las columnas de los resultados obtenidos
             attach(simulacionM2SI.si)
-            #Representamos graficamente los resultados obtenidos
+            #Gráficas
             plot(times, S, type="l", col="blue", ylim=c(0,sum(init)), xlab="Tiempo (en días)", ylab="Numero de host", main = "Metodo 2: Euler")
             lines(times, I, type="l", col="red")
-            #title("Modelo SI")
             legend(x = "topright", legend=c("Susceptibles", "Infectados"), col=c("blue", "red"), lty=rep(1, 2)) 
             
         })
         
         output$distPlotERROR <- renderPlot({
             tf = input$tiempodias
-            init <- c(S = input$poblacion , # Suceptibles 
-                      I = input$infectados)  # Infectados 
+            init <- c(S = input$poblacion , #Suceptibles 
+                      I = input$infectados)  #Infectados 
             
             param<-c(beta = input$transmision)
-            #crear la funcion con las ODE
             si <- function(times, init, param) 
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- - beta*S*I/(S+I)
-                         dI <- beta*S*I/(S+I)
-                         #resultados de las tasas de cambio    
+                         dI <- beta*S*I/(S+I)   
                          return(list(c(dS, dI)))
                      })
             }
             
-            #intervalo de tiempo y resolucion
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
-            simulacionM1SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "rk4"))
-            #simulacionM1SI.si
-            
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
+            simulacionM1SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "rk4")
             simulacionM2SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "euler"))
-            #simulacionM2SI.si
-            
-            # SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+
             i =1
             erroresM1S <- c()
             for (x in simulacionM1SI.si$S) {
-                #En M1, el teorico es M2
                 erroresM1S <- c(erroresM1S, ( (abs(simulacionM2SI.si$S[i]-x) )/simulacionM2SI.si$S[i] ) *100)
                 i = i +1;
             }
@@ -195,38 +175,29 @@ server <- function(input, output, session) {
         
         output$distPlot2ERROR <- renderPlot({
             tf = input$tiempodias
-            init <- c(S = input$poblacion , # Suceptibles
-                      I = input$infectados)  # Infectados 
+            init <- c(S = input$poblacion , #Suceptibles
+                      I = input$infectados)  #Infectados 
             
-            #parametros del modelo (coeficientes de las variables)
-            param <- c( beta = input$transmision) # Tasa de infeccion ( contagiados por unidad de tiempo) input$bethaSI
-            #crear la funcion con las ODE
+            #Párametros del modelo
+            param <- c( beta = input$transmision) #Tasa de infeccion
             si <- function(times, init, param) 
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- -beta*S*I/(S+I)
                          dI <- beta*S*I/(S+I)
-                         #resultados de las tasas de cambio    
                          return(list(c(dS, dI)))
                      })
             }
             
-            #intervalo de tiempo y resolucion
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM1SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "rk4"))
-            #simulacionM1SI.si
-            
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM2SI.si <- as.data.frame(ode(y=init, times=times, func=si,parms=param,method = "euler"))
-            #simulacionM2SI.si
             
             i =1
             erroresM1I <- c()
             for (x in simulacionM1SI.si$I) {
-                #En M1, el teorico es M2
                 erroresM1I <- c(erroresM1I, ( (abs(simulacionM2SI.si$I[i]-x) )/simulacionM2SI.si$I[i] ) *100 )
                 i = i +1;
             }
@@ -234,46 +205,38 @@ server <- function(input, output, session) {
             x <- seq(1,701)
             plot(simulacionM1SI.si$time , erroresM1I, col="red", type="l", xlab="Tiempo (en días)", ylab="Error relativo", main = "Error", ylim = c(0,100))
             
-        })
-        
+        })       
     }
     
     calcularEDOSIR <- function(){
         output$distPlot <- renderPlot({
             tf = input$tiempodias
-            #ESTADO INICIAL, mover deslizadores solo cambia estos valores, lo iniciales. 
-            init <- c(S = input$poblacion , # Susceptibles
-                      I = input$infectados,  # Infectados
-                      R = input$recuperados) # Recuperados
+            init <- c(S = input$poblacion , #Susceptibles
+                      I = input$infectados,  #Infectados
+                      R = input$recuperados) #Recuperados
             
-            param<-c(beta = input$transmision, #Hay que cambiar esto para que se ingrese con valores de los deslizadores. TOCA INVESTIGAR QUE VALORES SON MASO REALES. 
+            param<-c(beta = input$transmision, 
                      gamma = input$recuperacion)
-            #crear la funcion con las ODE.TOCA MIRAR SI ESTAN BIEN, Ellos interpretan gamma como probabilidad de infeccion y Wikipedia lo define como tiempo promedio de infeccion.
-            sir <- function(times, init, param) #Por eso toca mirar bien como se estan construyendo esas ED
+            sir <- function(times, init, param)
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- - beta*S*I/(S+I+R)
                          dI <- beta*S*I/(S+I+R) - gamma*I
                          dR <- gamma*I
-                         #resultados de las tasas de cambio    
                          return(list(c(dS, dI, dR)))
                      })
             }
             
-            #intervalo de tiempo y resolucion
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM1SI.sir <- as.data.frame(ode(y=init, times=times, func=sir,parms=param,method = "rk4"))      #Aqui se solucionan, mejor DEJAR ESTAS TRES LINEAS QUIETAS
-            #Esta orden nos permite hacer referencia de manera directa a #las columnas de los resultados obtenidos
             attach(simulacionM1SI.sir)
             
-            #Representamos graficamente los resultados obtenidos CON ESTO SI PODEMOS JUGAR UN POCO MAS, HAY QUE CAMBIAR LOS TITULOS Y ESAS COSAS. 
+            #Gráfica
             plot(times, S, type="l", col="blue", ylim=c(0,sum(init)), xlab="Tiempo (en días)", ylab="Numero de host",main = "Metodo 1: Runge Kutta 4")
             lines(times, I, type="l", col="red")
             lines(times, R, type="l", col="green")
-            #title("Modelo SIR")
             legend(x = "topright", legend=c("Susceptibles", "Infectados", "Recuperados"), col=c("blue", "red", "green"), lty=rep(1, 2, 3)) 
         })
         
@@ -286,73 +249,59 @@ server <- function(input, output, session) {
             
             param<-c(beta = input$transmision,
                      gamma = input$recuperacion)
-            #crear la funcion con las ODE
+            #Crear la funcion con las ODE
             sir <- function(times, init, param) 
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- - beta*S*I/(S+I+R)
                          dI <- beta*S*I/(S+I+R) - gamma*I
                          dR <- gamma*I
-                         #resultados de las tasas de cambio    
                          return(list(c(dS, dI, dR)))
                      })
             }
-            
-            #intervalo de tiempo y resolucion
+
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM2SI.sir <- as.data.frame(ode(y=init, times=times, func=sir, parms=param, method = "euler"))
-            #Esta orden nos permite hacer referencia de manera directa a #las columnas de los resultados obtenidos
             attach(simulacionM2SI.sir)
-            #Representamos graficamente los resultados obtenidos
+            #Grafica
             plot(times, S, type="l", col="blue", ylim=c(0,sum(init)), xlab="Tiempo (en días)", ylab="Numero de host", main = "Metodo 2: Euler")
             lines(times, I, type="l", col="red")
             lines(times, R, type="l", col="green")
-            #title("Modelo SIR")
             legend(x = "topright", legend=c("Susceptibles", "Infectados", "Recuperados"), col=c("blue", "red"), lty=rep(1, 2)) 
             
         })
         
         output$distPlotERROR <- renderPlot({
             tf = input$tiempodias
-            init <- c(S = input$poblacion , # Suceptibles 
-                      I = input$infectados,  # Infectados 
-                      R = input$recuperados) # Recuperados
+            init <- c(S = input$poblacion , #Suceptibles 
+                      I = input$infectados,  #Infectados 
+                      R = input$recuperados) #Recuperados
             
             param<-c(beta = input$transmision,
                      gamma = input$recuperacion)
-            #crear la funcion con las ODE
+            #Crear la funcion con las ODE
             sir <- function(times, init, param) 
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- - beta*S*I/(S+I+R)
                          dI <- beta*S*I/(S+I+R) - gamma*I
                          dR <- gamma*I
-                         #resultados de las tasas de cambio    
                          return(list(c(dS, dI, dR)))
                      })
             }
             
-            #intervalo de tiempo y resolucion
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM1SI.sir <- as.data.frame(ode(y=init, times=times, func=sir,parms=param,method = "rk4"))
-            #simulacionM1SI.sir
-            
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM2SI.sir <- as.data.frame(ode(y=init, times=times, func=sir,parms=param,method = "euler"))
-            #simulacionM2SI.sir
             
-            # SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
             i =1
             erroresM1S <- c()
             for (x in simulacionM1SI.sir$S) {
-                #En M1, el teorico es M2
-                erroresM1S <- c(erroresM1S, ( (abs(simulacionM2SI.sir$S[i]-x) )/simulacionM2SI.sir$S[i] ) *100)
+                erroresM1S <- c(erroresM1S, ( (abs(simulacionM2SI.sir$S[i]-x) )/simulacionM2SI.sir$S[i] ) * 100)
                 i = i +1;
             }
             x <- seq(1,70)
@@ -362,41 +311,33 @@ server <- function(input, output, session) {
         
         output$distPlot2ERROR <- renderPlot({
             tf = input$tiempodias
-            init <- c(S = input$poblacion , # Suceptibles
-                      I = input$infectados,  # Infectados
-                      R = input$recuperados) # Recuperados
+            init <- c(S = input$poblacion , #Suceptibles
+                      I = input$infectados,  #Infectados
+                      R = input$recuperados) #Recuperados
             
-            #parametros del modelo (coeficientes de las variables)
-            param <- c( beta = input$transmision, # Tasa de infeccion ( contagiados por unidad de tiempo) input$bethaSI
+            #Parámetros del modelo 
+            param <- c( beta = input$transmision, #Tasa de infección
                         gamma = input$recuperacion)
-            #crear la funcion con las ODE
+            #Crear la funcion con las ODE
             sir <- function(times, init, param) 
             {
                 with(as.list(c(init, param)), 
                      {
-                         #ecuaciones diferenciales   
+                         #Ecuaciones diferenciales   
                          dS <- - beta*S*I/(S+I+R)
                          dI <- beta*S*I/(S+I+R) - gamma*I
                          dR <- gamma*I
-                         #resultados de las tasas de cambio    
                          return(list(c(dS, dI, dR)))
                      })
-            }
-            
-            #intervalo de tiempo y resolucion
+            }    
+
             times <- seq(0, tf, by = 0.1)
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM1SI.sir <- as.data.frame(ode(y=init, times=times, func=sir,parms=param,method = "rk4"))
-            #simulacionM1SI.si
-            
-            #Mediante la funcion "ode" resolvemos el sistema de ecuaciones diferenciales y generamos un data frame 
             simulacionM2SI.sir <- as.data.frame(ode(y=init, times=times, func=sir,parms=param,method = "euler"))
-            #simulacionM2SI.si
             
             i =1
             erroresM1I <- c()
             for (x in simulacionM1SI.sir$I) {
-                #En M1, el teorico es M2
                 erroresM1I <- c(erroresM1I, ( (abs(simulacionM2SI.sir$I[i]-x) )/simulacionM2SI.sir$I[i] ) *100 )
                 i = i +1;
             }
@@ -408,7 +349,6 @@ server <- function(input, output, session) {
         
     }
 }
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
